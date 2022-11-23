03b71902260ed761568d249f56a610dbd9eb673e
Artem Belankov <temanmd@gmail.com>
2022-11-23 15:56:01 +0600

    Add resources/public/js to gitignore

--- resources/public/js/cljs-runtime/shadow.module.app.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('app');
-
-try { foobar.frontend.app.init(); } catch (e) { console.error("An error occurred when calling (foobar.frontend.app/init)"); throw(e); }
